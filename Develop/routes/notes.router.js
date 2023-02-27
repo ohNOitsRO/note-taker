eb77a5102ef7a29d9e1a0a60bec28c06caaff022
Delgado <rxd718@miami.edu>
2023-02-27 13:30:54 -0500

    WIP router code

--- Develop/routes/notes.router.js
+++ Develop/routes/notes.router.js
@@ -1,50 +1,20 @@
-const notes = require('express').Router();
+const notesRoute = require('express').Router();
 const { createNewNote, deleteNote } = require('../lib/notes');
 let { notesArray } = require('../lib/notes');
 
 
+
 // GET Route for retrieving all the notes
-// notes.get('/', (req, res) =>
-//   readFromFile('/').then((data) => res.json(JSON.parse(data)))
-// );
 
-
-notes.get('/', (req, res) => {
+notesRoute.get('/notes', (req, res) => {
     let results = notesArray;
     res.json(results);
 });
 
 
 // // POST Route for submitting notes
-// notes.post('/', (req, res) => {
-//   // Destructuring assignment for the items in req.body
-//   const { title, text } = req.body;
 
-//   // If all the required properties are present
-//   if (title && text) {
-//     // Variable for the object we will save
-//     const newNote = {
-//       title,
-//       text,
-//     };
-
-//     readAndAppend(newNote, './Develop/db/db.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newNote,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting note!');
-//   }
-// });
-
-
-
-
-notes.post('/notes', (req, res) => {
+notesRoute.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
   if(notesArray){
   req.body.id = notesArray.length.toString();
@@ -57,10 +27,14 @@ notes.post('/notes', (req, res) => {
 
 });
 
-notes.delete('/notes/:id', async (req, res) => {
+
+// DELETE route for erasing previous notes
+
+notesRoute.delete('/notes/:id', async (req, res) => {
   const { id } = req.params
   notesArray = await deleteNote(id, notesArray);
   res.json(notesArray);
 });
+
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const fs = require('fs');
 const path = require('path');
-const apiRoutes = require('./routes/notes.router');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 
