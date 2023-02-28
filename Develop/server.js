d0626b836bff5cbb5318b6d4e1feef58cd3f0040
Delgado <rxd718@miami.edu>
2023-02-28 08:01:44 -0500

    Cleaning up code

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const notesRouter = require('./routes/notes.router');
 const htmlRouter = require('./routes/html.router');
@@ -7,13 +9,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static('public'));
 
-const app = express();
-
-app.use("/api", notesRouter);
-app.use("/", htmlRouter);
+app.use('/api', notesRouter);
+app.use('/', htmlRouter);
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`);
-});
+    console.log(`API server now live on port ${PORT}!`);
+  });
+
+module.exports = app;
